445ba532fa35fe044e1fd3fa574e50bd430b65ae
nue-os <kimsoeun0108@gmail.com>
2025-06-05 15:28:24 +0900

    Feat: FilterButton 컴포넌트

---
+++ components/common/FilterButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+interface FilterButtonProps {
+  text: string;
+  onClick: () => void;
+  isActive?: boolean;
+}
+
+export default function FilterButton({
+  text,
+  onClick,
+  isActive = false,
+}: FilterButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 rounded-full border transition-colors duration-200 outline-none text-[18px]
+        ${
+          isActive
+            ? "bg-[#11C891] text-white"
+            : "border-transparent hover:border-[#11C891] hover:bg-[#EFFFF4] hover:text-[#11C891]"
+        }`}
+    >
+      {text}
+    </button>
+  );
+}
