19f2f0cb0138ce0b8debdbea811321f3013d153d
nue-os <kimsoeun0108@gmail.com>
2025-06-09 23:52:45 +0900

    Style: FilterModal 제목 및 마진 적용

--- app/(home)/_components/filter/FilterModal.tsx
+++ app/(home)/_components/filter/FilterModal.tsx
@@ -12,22 +12,23 @@ export default function FilterModal({ filter, onApply }: FilterModalProps) {
   const renderFilterContent = () => {
     switch (filter.key) {
       case "type":
-        return <TypeFilter label={filter.label} />;
+        return <TypeFilter />;
       case "price":
-        return <div>{filter.label}</div>;
+        return <div></div>;
       case "area":
-        return <div>{filter.label}</div>;
+        return <div></div>;
       case "kind":
-        return <div>{filter.label}</div>;
+        return <div></div>;
       case "crop":
-        return <div>{filter.label}</div>;
+        return <div></div>;
       default:
         return <div>잘못된 필터입니다.</div>;
     }
   };
   return (
     <div className="bg-white rounded-[20px] shadow-[0px_0px_20px_0px_rgba(0,0,0,0.08)] p-5 min-w-[380px]">
-      <div className="mb-3">{renderFilterContent()}</div>
+      <h2 className="text-lg font-extrabold">{filter.label}</h2>
+      <div className="flex flex-col my-6">{renderFilterContent()}</div>
       <div className="flex justify-between items-center gap-2 text-sm border-t border-gray-400 pt-[10px]">
         <button
           type="button"
