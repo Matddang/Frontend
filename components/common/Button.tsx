31a7a5198db8c14a228a8b066b8ac5c9673df977
nue-os <kimsoeun0108@gmail.com>
2025-06-09 23:39:56 +0900

    Feat: Button 공통 컴포넌트

---
+++ components/common/Button.tsx
@@ -0,0 +1,21 @@
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+  isActive?: boolean;
+}
+export default function Button({ text, onClick, isActive }: ButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`w-full px-[34px] py-3 rounded-[10px] border-[2px] transition-colors duration-200 text-lg
+        ${
+          isActive
+            ? "border-primary text-primary"
+            : "border-gray-400 hover:border-primary hover:text-primary"
+        }`}
+    >
+      {text}
+    </button>
+  );
+}
