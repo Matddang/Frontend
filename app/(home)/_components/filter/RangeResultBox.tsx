b719d8b13d697ba8019c55cb47398e6468ba9e52
nue-os <kimsoeun0108@gmail.com>
2025-06-12 20:35:39 +0900

    Feat: 희망가가 1억 이상인 경우 서브 라벨 출력

--- app/(home)/_components/filter/RangeResultBox.tsx
+++ app/(home)/_components/filter/RangeResultBox.tsx
@@ -1,27 +1,31 @@
+import { formatKoreanUnit } from "@/utils/format";
+
 export default function RangeResultBox({
   title,
   maxValue,
   value,
   unit,
   label,
-  subLabel,
 }: {
   title: string;
   maxValue?: number;
   value: number;
   unit?: number;
   label: string;
-  subLabel?: string;
 }) {
   const displayValue = unit ? value / unit : value;
   const isMax = maxValue === value;
+
+  const formmatedSubLabel = formatKoreanUnit(value) + (isMax ? "이상" : "");
   const formattedValue = displayValue.toLocaleString() + (isMax ? "+" : "");
 
   return (
     <div className="flex flex-col gap-[6px]">
       <div className="flex justify-between typo-sub-title-m">
         <span className="text-primary">{title}</span>
-        <span className="text-gray-900">{subLabel}</span>
+        {value >= 100000000 ? (
+          <span className="text-gray-900">{formmatedSubLabel}</span>
+        ) : null}
       </div>
       <div className="flex justify-between bg-gray-100 border border-gray-400 rounded-[10px] px-5 py-3">
         <span>{formattedValue}</span>
