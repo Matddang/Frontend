60e876e442e8d1e3c467864b5d9fcc9b4a5e528c
nue-os <kimsoeun0108@gmail.com>
2025-06-13 21:39:38 +0900

    Feat: 공통 버튼 컴포넌트 active 상태일 때 스타일 props로 받도록 지정

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -2,14 +2,21 @@ interface ButtonProps {
   text: string;
   onClick: () => void;
   isActive?: boolean;
+  activeColor?: [string, string, string]; // border, background, text
 }
-export default function Button({ text, onClick, isActive }: ButtonProps) {
+
+export default function Button({
+  text,
+  onClick,
+  isActive,
+  activeColor = ["border-primary", "bg-white", "text-primary"],
+}: ButtonProps) {
   return (
     <button
       type="button"
       onClick={onClick}
       className={`w-full py-3 rounded-[10px] border-[2px] transition-colors duration-200 text-lg
-        ${isActive ? "border-primary text-primary" : "border-gray-400"}`}
+        ${isActive ? `${activeColor.join(" ")}` : "border-gray-400"}`}
     >
       {text}
     </button>
