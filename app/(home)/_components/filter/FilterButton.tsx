d6eb814bcbac00009261b12ba73836e3b8d9757e
nue-os <kimsoeun0108@gmail.com>
2025-06-15 20:56:50 +0900

    Feat: 필터 값을 가지고 있는 경우 버튼 스타일 처리

--- app/(home)/_components/filter/FilterButton.tsx
+++ app/(home)/_components/filter/FilterButton.tsx
@@ -4,23 +4,28 @@ interface FilterButtonProps {
   text: string;
   onClick: () => void;
   isActive?: boolean;
+  hasValue?: boolean;
 }
 
 export default function FilterButton({
   text,
   onClick,
   isActive = false,
+  hasValue = false,
 }: FilterButtonProps) {
+  const baseClass =
+    "px-4 py-2 rounded-full border transition-colors duration-200 outline-none text-[18px] cursor-pointer";
+  const activeClass = "border-primary bg-primary-light text-primary";
+  const hasValueClass = "bg-primary text-white";
+  const defaultClass =
+    "border-transparent hover:border-primary hover:bg-primary-light hover:text-primary";
+
+  const className = `${baseClass} ${
+    isActive ? activeClass : hasValue ? hasValueClass : defaultClass
+  }`;
+
   return (
-    <button
-      onClick={onClick}
-      className={`px-4 py-2 rounded-full border transition-colors duration-200 outline-none text-[18px] cursor-pointer
-        ${
-          isActive
-            ? "border-primary bg-primary-light text-primary"
-            : "border-transparent hover:border-primary hover:bg-primary-light hover:text-primary"
-        }`}
-    >
+    <button onClick={onClick} className={className}>
       {text}
     </button>
   );
