fa6d4a7b15cc158c1ac085bdad15b0ad6022919d
nue-os <kimsoeun0108@gmail.com>
2025-06-18 12:31:26 +0900

    Style: 후버 스타일 변경

--- app/(home)/_components/filter/FilterButton.tsx
+++ app/(home)/_components/filter/FilterButton.tsx
@@ -14,11 +14,10 @@ export default function FilterButton({
   hasValue = false,
 }: FilterButtonProps) {
   const baseClass =
-    "px-4 py-2 rounded-full border transition-colors duration-200 outline-none text-[18px] cursor-pointer";
+    "px-4 py-2 typo-sub-head-sb rounded-full border transition-colors duration-200 outline-none text-[18px] cursor-pointer";
   const activeClass = "border-primary bg-primary-light text-primary";
   const hasValueClass = "bg-primary text-white";
-  const defaultClass =
-    "border-transparent hover:border-primary hover:bg-primary-light hover:text-primary";
+  const defaultClass = "border-transparent hover:text-gray-600";
 
   const className = `${baseClass} ${
     isActive ? activeClass : hasValue ? hasValueClass : defaultClass
