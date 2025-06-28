8af07e9d2a772d957ab111778e1ac1df2f06549c
nue-os <kimsoeun0108@gmail.com>
2025-06-29 00:50:49 +0900

    Fix: 랭킹 매물 찜 동기화 안되는 문제 토큰 전달로 수정

--- services/getProperties.ts
+++ services/getProperties.ts
@@ -1,12 +1,20 @@
+import { useTokenStore } from "@/store/useTokenStore";
 import axios from "axios";
 
 export async function getProperties(sortBy: string) {
+  const { token } = useTokenStore.getState();
+
   try {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_BASE_URL}/sale`,
       {
         sortBy,
         size: 5,
+      },
+      {
+        headers: {
+          Authorization: `${token}`,
+        },
       },
     );
 
